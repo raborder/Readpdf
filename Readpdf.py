f7e6636b3afe668491df7de21fe19e81ceabcaa9
raborder <russell.border@boxhill.edu.au>
2026-02-01 00:23:09 +1100

    Start to read IC32G

--- Readpdf.py
+++ Readpdf.py
@@ -1,7 +1,9 @@
+from ast import Try
 from pypdf import PdfReader
+import re
 
 # Create a PdfReader object by providing the path to your PDF file
-reader = PdfReader('CELCAT_Timetable_IC32V.pdf')
+reader = PdfReader('CELCAT_Timetable_IC32G.pdf')
 
 # Get the total number of pages
 num_pages = len(reader.pages)
@@ -10,11 +12,58 @@ print(f"Total pages: {num_pages}")
 # Extract text from a specific page (e.g., the first page, which is index 0)
 page = reader.pages[0]
 text = page.extract_text()
-print(text)
+# print(text)
 
 # To extract text from all pages, you can loop through them
 all_text = ""
 for page in reader.pages:
     all_text += page.extract_text() + "\n"
-# print(all_text)
+print(all_text)
 
+pattern = r'^(Mon|Tue|Wed|Thu|Fri|Sat|Sun)\b'
+
+lines = all_text.splitlines()
+
+for i, line in enumerate(lines):       # skip preliminary lines at start
+    regex = re.compile(pattern, re.MULTILINE)   # look for day of week
+    match = regex.match(line)
+    if match != None:
+        day = match
+        print(match)
+        start_time = line[5:12]
+        print(start_time)
+        stop_time = line[13:]
+        print(stop_time.strip())
+        i += 1
+        try:
+            line = lines[i]     # checking for case where day of week was on last line and therefore can't increment.  This may need to change as develop code
+            if "Wks" in line:           # Multiple week event
+                pass
+            else:                       # Must be single week event
+                date = line[-10:]
+                print(date)
+                i += 1
+                line = lines[i]
+                summary = line
+                print(summary)
+        except:
+            print("something screwed up")
+
+        
+
+
+
+
+"""
+for line in lines:
+    regex = re.compile(pattern, re.MULTILINE)
+    match = regex.match(line)
+    print(match)
+    day = match
+    if match != None:
+        start_time = line[5:12]
+        print(start_time)
+        stop_time = line[13:]
+        print(stop_time)
+    line = 
+"""
