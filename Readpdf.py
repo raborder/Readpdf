d5240d994d46bd866ad30a635889060bf2f0bb71
raborder <russell.border@boxhill.edu.au>
2026-02-04 00:38:43 +1100

    more loop

--- Readpdf.py
+++ Readpdf.py
@@ -25,7 +25,7 @@ pattern = r'^(Mon|Tue|Wed|Thu|Fri|Sat|Sun)\b'
 lines = all_text.splitlines()
 
 for i, line in enumerate(lines):       # skip preliminary lines at start
-    regex = re.compile(pattern, re.MULTILINE)   # look for day of week
+    regex = re.compile(pattern)   # look for day of week
     match = regex.match(line)
     if match != None:
         day = match
@@ -55,19 +55,34 @@ for i, line in enumerate(lines):       # skip preliminary lines at start
             while gathering_info:
                 match (line.split(":")[0]):
                     case "Availabilities":
-                        print("Availabilities") # To do
-                    case "Rooms":
+                        parameter_found = False     # Loop around until next parameter, building up Availabilities string. Line with 'Availabilities' cannot be next parameter
+                        parameter = line
+                        while parameter_found == False:
+                            i += 1
+                            line = lines[i]
+                            parameter_pattern = r'^(Rooms:|Staff:|Activities:|Courses:|Notes:)\b'
+                            regex = re.compile(pattern, re.MULTILINE)   # look for day of week
+                            match = regex.match(parameter)
+                            if match != None:       # must have moved to next parameter
+                                break
+                            else:
+                                parameter = parameter + line
+                        unit_pattern = r'VU\d{5}'|'BSB\c{3}\d{3}'|'ICT\c{3}\d{3}'   # search for unit using regex
+                        regex = re.compile(unit_pattern)   # look for unit
+                        availabilities = regex.match(line)
+                        print(availabilities)
+                    case "Rooms":                   # Check if multiple lines
                         room = line[:6].strip()
                         print(room)
-                    case "Classes":
+                    case "Classes":                 # Should be OK, 1 line only
                         group = line[0]
                         print(group)
                     case "Staff":
                         teacher = line
-                        print(teacher)
+                        print(teacher)              # Should be OK, 1 line only
                     case "Activities":
-                        pattern = r'VU\d{5}'|'BSB\c{3}\d{3}'|'ICT\c{3}\d{3}'
-                        regex = re.compile(pattern)   # look for unit
+                        unit_pattern = r'VU\d{5}'|'BSB\c{3}\d{3}'|'ICT\c{3}\d{3}'
+                        regex = re.compile(unit_pattern)   # look for unit
                         activity = regex.match(line)
                         print(activity)     
                     case "Courses":
@@ -75,10 +90,12 @@ for i, line in enumerate(lines):       # skip preliminary lines at start
                         print(course)
                     case "Notes":               # event information extraction complete
                         gathering_info = False
-                i += 1
-                line = lines[i]
-                
+                    case _:
+                        i += 1
+                        line = lines[i]
+               
 
+            '''
             if 'Rooms' in line:             # Unit not listed
                 pass
             else:                           # Event must start with Availabilities
@@ -89,9 +106,9 @@ for i, line in enumerate(lines):       # skip preliminary lines at start
                     availabilitiies = availabilities + line
                     print(availabilities)
                 room = "B10." + line[11:13].strip()
+            '''
         except:
             print("something screwed up")
-
         
 
 
